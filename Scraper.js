32bed7a16cf23ddeac0053b638984dd1d2f8f08f
RealGenius1 <sptho24@gmail.com>
2024-10-19 15:13:48 -0400

    it's a function now, can't make it return properly but that's an issue for future me

--- Scraper.js
+++ Scraper.js
@@ -1,6 +1,6 @@
 import { connect } from 'puppeteer-real-browser'
 
-(async () => {
+async function  getData(ID)  {
     connect({
 
         headless: 'auto',
@@ -29,20 +29,30 @@ import { connect } from 'puppeteer-real-browser'
     })
     .then(async response => {
         const {browser, page} = response
-        await page.goto('https://www.barcodelookup.com/998390023207', {
+        await page.goto('https://www.barcodelookup.com/' + ID, {
             waitUntil: "domcontentloaded",
         });
             // Fetch the first element with class "quote"
             // Get the displayed text and returns it
             const quoteList = page.$eval('h4', text => text.textContent);
 
-        const val1 = await quoteList;
-        console.log(val1);
-        //   return quoteList.PromiseResult;
+            const val1 =  await quoteList;
+            
+
+       
+        console.log(await quoteList + "    AHAHGLKHKJLAKHG    "
+        );
+
+        await page.close();
+
+        return val1;
+
+        
         
         
     })
     .catch(error=>{
         console.log(error.message)
     })
-  })();
+  };
+  getData(998390023207);
