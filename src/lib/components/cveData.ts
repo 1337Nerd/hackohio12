7423fd9257903ff14651f928f7bcb29d1db47b67
1337Nerd <26494273+1337Nerd@users.noreply.github.com>
2024-10-20 02:25:21 +0000

    Remove store

--- src/lib/components/cveData.ts
+++ src/lib/components/cveData.ts
@@ -1,3 +1,2 @@
 import { writable, type Writable } from 'svelte/store'
 export const cveData: Writable<CVEList | { vendor: string, products: string[] }> = writable()
-export const currentView: Writable<'Scanner' | 'Welcome' | 'Selector' | 'Viewer'> = writable('Welcome')
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,23 @@
 	import '../app.css'
 	import Data from '$lib/components/Data.svelte'
 	import Selector from '$lib/components/Selector.svelte'
-	import { cveData, currentView } from '$lib/components/cveData'
+	import { cveData } from '$lib/components/cveData'
 	import Welcome from '$lib/components/Welcome.svelte'
-	import Video from '$lib/components/Video.svelte'
+	import Video from '$lib/components/Scanner.svelte'
+	let currentView: 'Scanner' | 'Welcome' | 'Selector' | 'Viewer' = 'Welcome'
 	function onCode(bool: boolean) {
 		if (bool)
-			$currentView = 'Viewer'
+			currentView = 'Viewer'
 		else
-			$currentView = 'Selector'
+			currentView = 'Selector'
 	}
 </script>
 
-{#if $currentView === 'Welcome'}
-	<Welcome onScan={() => $currentView = 'Scanner'} />
-{:else if $currentView === 'Scanner'}
+{#if currentView === 'Welcome'}
+	<Welcome onScan={() => currentView = 'Scanner'} />
+{:else if currentView === 'Scanner'}
 	<Video {onCode} />
-{:else if $currentView === 'Viewer'}
+{:else if currentView === 'Viewer'}
 	<Data cves={$cveData} />
 {:else}
 	<Selector vendor={$cveData.vendor} products={$cveData.products} />
