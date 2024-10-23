a8f142e6510db3c4926aae67434cf09fa88981f0
1337Nerd <26494273+1337Nerd@users.noreply.github.com>
2024-10-23 17:50:51 +0000

    Fix not correctly encoding URI components

--- src/routes/api/product/[vendor]/+server.ts
+++ src/routes/api/product/[vendor]/+server.ts
@@ -2,7 +2,9 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 
 export const GET: RequestHandler = async({ params, fetch }) => {
 	const { vendor } = params
-	const res = await fetch(`https://vulnerability.circl.lu/api/browse/${vendor?.toLowerCase()}`)
+	if (!vendor)
+		return new Response(null, { status: 404 })
+	const res = await fetch(`https://vulnerability.circl.lu/api/browse/${encodeURIComponent(vendor?.toLowerCase())}`)
 	const data = await res.json()
 	return json(data)
 }
--- src/routes/api/product/[vendor]/[product]/+server.ts
+++ src/routes/api/product/[vendor]/[product]/+server.ts
@@ -2,7 +2,9 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 
 export const GET: RequestHandler = async({ params, fetch }) => {
 	const { vendor, product } = params
-	const res = await fetch(`https://vulnerability.circl.lu/api/search/${vendor?.toLowerCase()}/${product?.replace(/\s/g, '_').toLowerCase()}`)
+	if (!vendor || !product)
+		return new Response(null, { status: 404 })
+	const res = await fetch(`https://vulnerability.circl.lu/api/search/${encodeURIComponent(vendor?.toLowerCase())}/${encodeURIComponent(product?.replace(/\s/g, '_').toLowerCase())}`)
 	const data: CVEList = await res.json()
 	data.cvelistv5.map(cve => cve[1].vendor = vendor ?? '')
 	return json(data)
