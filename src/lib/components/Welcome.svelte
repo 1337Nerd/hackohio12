364cbf056f34727dd0cf86320fb5c316c1462fb6
1337Nerd <26494273+1337Nerd@users.noreply.github.com>
2024-10-23 17:40:17 +0000

    Fix some issues with hydration

--- src/lib/components/Welcome.svelte
+++ src/lib/components/Welcome.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import logo from '$lib/riskBustersTransparent.png?url'
+	import type { Action } from 'svelte/action'
 	export let onScan
+	const hydrate: Action<HTMLButtonElement> = (node) => {
+		node.disabled = false
+	}
 </script>
 <div class="flex flex-col items-center justify-center min-h-svh bg-white m-4">
 	<img src={logo} alt="riskBuster Logo" class="mb-8 w-72 h-auto aspect-[741/490]" />
@@ -8,7 +12,7 @@
 	<p class="text-gray-700 mb-6 text-center">
 		Scan a barcode to identify hardware vulnerabilities. Press the button below to launch the camera.
 	 </p>
-	 <button on:click={onScan} class="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 transition duration-300">
+	 <button use:hydrate on:click={onScan} disabled class="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md enabled:hover:bg-red-700 transition duration-300">
 		Start Camera
 	</button>
 </div>
